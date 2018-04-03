12106762238efb5719b220278eaea881ae4a5d96
Benjamin Ang <benjamin.ang@gmail.com>
2018-04-03 10:26:25 +0800

    added test directory

--- test/isPalindrome.test.js
+++ test/isPalindrome.test.js
@@ -1,3 +1,4 @@
+let isPalindrome = require("../src/isPalindrome.js");
 // Test cases: actual --> expected
 // isPalindrome('') --> true
 // isPalindrome('a') --> true
@@ -7,3 +8,20 @@
 // isPalindrome('tacocat') --> true
 // isPalindrome('racecar') --> true
 // isPalindrome('umbrella') --> false
+
+describe("Palindrom Test", () => {
+  it("should be true when reverse", () => {
+    let input = "a";
+    expect(isPalindrome(input)).toEqual(true);
+  });
+
+  it("should return true when input is more than one char", () => {
+    let input = "eve";
+    expect(isPalindrome(input)).toEqual(true);
+  });
+
+  it("should return false when input is not a palindrome", () => {
+    let input = "abc";
+    expect(isPalindrome(input)).toEqual(false);
+  });
+});
