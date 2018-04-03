706465140eff01e85c658370db737056f086a944
Benjamin Ang <benjamin.ang@gmail.com>
2018-04-03 10:21:40 +0800

    added PASSED TDD cases

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -4,6 +4,12 @@ A palindrome is a word, phrase, number, or other sequence of characters which
 reads the same backward as forward, such as madam or racecar. 
 
 `input` will be a single string without spaces and punctuation. */
-const isPalindrome = input => {};
+const isPalindrome = input => {
+  let inputReverse = input.split("").reverse().join("");
+
+  return inputReverse === input;
+};
+
+console.log(isPalindrome(""));
 
 module.exports = isPalindrome;
