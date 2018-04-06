8890d021ccad1ca14ddfd21d8abcc58a98918770
Victor Lee <myleevictor@gmail.com>
2018-04-06 10:26:44 +0800

    passed test

--- src/decodeMessage.js
+++ src/decodeMessage.js
@@ -3,9 +3,24 @@ Each letter is paired with the letter that it coincides with when the alphabet i
 For example: "a" is encoded with "z", "b" with "y", "c" with "x", etc
 Spaces are not encoded/decoded. For example: "gsrh nvhhztv" will be decoded as "this message"
 */
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-const decodeMessage = (string) => {
+const decodeMessage = string => {
+  const stringArr = string.split("");
+  const resultArr = [];
 
+  for (let i = 0; i < stringArr.length; i++) {
+    if (stringArr[i] === " ") resultArr.push(stringArr[i]);
+    if (alphabet.includes(stringArr[i].toLowerCase())) {
+      const indexOfString = alphabet
+        .split("")
+        .indexOf(stringArr[i].toLowerCase());
+      const result = alphabet.length - 1 - indexOfString;
+      resultArr.push(alphabet[result]);
+    }
+  }
+
+  return resultArr.join("");
 };
 
 module.exports = decodeMessage;
--- test/decodeMessage.test.js
+++ test/decodeMessage.test.js
@@ -1,3 +1,27 @@
+const decodeMessage = require('../src/decodeMessage');
+
+test("decodeMessage('a') will return z", () => {
+    expect(decodeMessage("y")).toEqual("b");
+});
+
+test("decodeMessage('abc') will return zyx", () => {
+  expect(decodeMessage("abc")).toEqual("zyx");
+});
+
+
+test("decodeMessage('a b c') will return z y x", () => {
+  expect(decodeMessage("a bc")).toEqual("z yx");
+});
+
+test("decodeMessage('r slkv mlylwb wvxlwvh gsrh nvhhztv') will return i hope nobody decodes this message", () => {
+  expect(decodeMessage("r slkv mlylwb wvxlwvh gsrh nvhhztv")).toEqual("i hope nobody decodes this message");
+});
+
+test("decodeMessage('r sLKv) will return i hope", () => {
+  expect(decodeMessage("r sLKv")).toEqual(
+    "i hope"
+  );
+});
 // Test cases: actual --> expected
 // decodeMessage("a") --> "z"
 // decodeMessage("z") --> "a"
